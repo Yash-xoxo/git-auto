55c8de16e991f24bea5da6ca0456a99d4d9f7662
Yash-xoxo <satorufxd@gmail.com>
2026-04-10 06:14:08 +0000

    script NISps

--- rn.sh
+++ rn.sh
@@ -2,6 +2,9 @@
 
 read -p "MSG: " msg
 
+# generate random 5-character string
+rand=$(tr -dc A-Za-z </dev/urandom | head -c 5)
+
 git add .
-git commit -m '$msg'
+git commit -m "$msg $rand"
 git push origin main
